cf49d8252adf7563148fce65bc880db6c0fea829
Danny <decunez@espe.edu.ec>
2026-08-28 23:35:26 -0500

    update 1
    update

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Service Worker mínimo para habilitar instalación
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Manejo básico de peticiones
+});
